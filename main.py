8b7b259e7ab59ff18e5ff02fa12c48ad17928a7b
Sergio Rios <sergiorios@javeriana.edu.co>
2025-07-27 17:00:03 -0500

    Added theme to make it dark mode

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from tkinter import *
 from tkinter import ttk
 from functions import *
+from ttkthemes import ThemedTk
 root = Tk()
+big_frame = ttk.Frame(root)
+big_frame.pack(fill="both", expand=True)
 root.title("API on call button")
+root.tk.call("source", "azure.tcl")
+root.tk.call("set_theme", "dark")
 
 result_label = ttk.Label(root, text="Waiting...")
 result_label.pack(pady=10)
